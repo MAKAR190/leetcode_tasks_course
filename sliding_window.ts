d1d3d08f0c62106c309f1a3dcfa8cdcf765385ad
Makar190 <lutskyimakar@gmail.com>
2023-07-01 13:33:43 +0200

    pushed

--- sliding_window.ts
+++ sliding_window.ts
@@ -35,3 +35,29 @@ function longestOnes(nums: number[], k: number): number {
   }
   return ans;
 }
+
+function getAverages(nums: number[], k: number): number[] {
+  if (k === 0) {
+    return nums;
+  }
+
+  let avgs = new Array(nums.length).fill(-1);
+
+  if (2 * k + 1 > nums.length) {
+    return avgs;
+  }
+
+  let windowSum = 0;
+  for (let i = 0; i < 2 * k + 1; i++) {
+    windowSum += nums[i];
+  }
+
+  avgs[k] = Math.floor(windowSum / (2 * k + 1));
+
+  for (let i = 2 * k + 1; i < nums.length; i++) {
+    windowSum = windowSum - nums[i - 2 * k - 1] + nums[i];
+    avgs[i - k] = Math.floor(windowSum / (2 * k + 1));
+  }
+
+  return avgs;
+}
